242b6479b8c4d170d5ea35238da6250fbece4e38
SeungWooChoi <89066816+Seawo@users.noreply.github.com>
2024-02-15 14:59:44 +0900

    Update HitUP.Build.cs

--- HitUP_Online/Source/HitUP_Online/HitUP.Build.cs
+++ HitUP_Online/Source/HitUP_Online/HitUP.Build.cs
@@ -7,9 +7,7 @@ public class HitUP : ModuleRules
 	public HitUP(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Sockets", "Networking", "AIModule", "NavigationSystem" });
-
+      
         PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
     }
 	
